dd550d1bc906e2c4ab59ca6e90e25a6a8413cced
masa <masa.swdan@gmial.com>
2025-09-25 21:45:27 +0300

    commited out folder

---
+++ portfolio2/portfolio/out/_next/static/chunks/3ccf1b53fc87a461.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/f30dfddf8ce4c920.js",
+  "static/chunks/turbopack-fc44cb195d7def68.js"
+])
---
+++ portfolio2/portfolio/out/_next/static/chunks/3e98c0fbff14c0d6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/f30dfddf8ce4c920.js",
+  "static/chunks/turbopack-d4d717394e4824b5.js"
+])
