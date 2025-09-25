74164a7e9678e7a7ee5a6cd54903eac7880e4c15
masa <masa.swdan@gmial.com>
2025-09-25 21:51:30 +0300

    fixed next config

--- portfolio2/portfolio/next.config.mjs
+++ portfolio2/portfolio/next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/portfolio2", // remove if deploying at root (MasaSN.github.io)
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig; // ✅ ESM export
+export default nextConfig;
